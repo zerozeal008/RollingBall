600bdde9834f1ab0bd66f908ce0c8917ab746424
Kritsana Treechalong <kritsana.tre@ku.th>
2025-06-26 01:57:34 +0700

    StarterProgram

---
+++ Lib/Form1.java
@@ -0,0 +1,16 @@
+package Lib;
+import javax.swing.*;
+
+public class Form1 extends JFrame {
+    public Form1(){
+        //this.getContentPane().add(new RollingBall());
+        this.setSize(300,300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
+
+
+
+
---
+++ Main.java
@@ -0,0 +1,7 @@
+import Lib.Form1;
+
+public class Main{
+    public static void main(String[] args) {
+        new Form1();
+    }
+}
